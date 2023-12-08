dbdcbb1852e9e341916b97ade4637ce585cefa03
Thistlecrux <swhitfie@unca.edu>
2023-12-08 15:09:33 -0500

    updates

---
+++ quizzes/quiz03(redo)/exercise01/main.js
@@ -0,0 +1,11 @@
+function changeBackground() {
+    document.querySelector("#s1").style.backgroundColor = "blue"
+}
+
+function goodbye() {
+    document.querySelector("#message").innerHTML="Goodbye!"
+}
+
+function showDog() {
+    document.querySelector("#animal1").src="images/dog.jpg"
+}
